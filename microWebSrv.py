425e665035fbee601f686c22107ade3dda0c76c0
jczic <jczic@free.fr>
2019-07-30 17:35:09 +0200

    Fonts mime type added

--- microWebSrv.py
+++ microWebSrv.py
@@ -57,11 +57,15 @@ class MicroWebSrv :
         ".json"  : "application/json",
         ".zip"   : "application/zip",
         ".pdf"   : "application/pdf",
+        ".woff"  : "application/font-woff",
+        ".woff2" : "application/font-woff2",
+        ".ttf"   : "application/x-font-ttf",
+        ".otf"   : "application/x-font-opentype",
         ".jpg"   : "image/jpeg",
         ".jpeg"  : "image/jpeg",
         ".png"   : "image/png",
         ".gif"   : "image/gif",
-        ".svg"   : "image/svg+xml",
+        ".svg"   : "image/svg+xml",        
         ".ico"   : "image/x-icon"
     }
 
